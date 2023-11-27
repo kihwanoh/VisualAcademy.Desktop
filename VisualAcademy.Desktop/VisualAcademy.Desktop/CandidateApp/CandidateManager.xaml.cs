848e6464ca9830ecb816a34eebb5d9d086278498
kihwanoh <kihwan2003.oh@hanmail.net>
2023-11-27 13:32:17 +0900

    CandidateApp Demos Images Rias 추가

---
+++ VisualAcademy.Desktop/VisualAcademy.Desktop/CandidateApp/CandidateManager.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace VisualAcademy.Desktop.CandidateApp
+{
+    /// <summary>
+    /// CandidateManager.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class CandidateManager : Window
+    {
+        public CandidateManager()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAddCandidate_Click(object sender, RoutedEventArgs e)
+        {
+            //MessageBox.Show("직원 후보자");
+            btnAddCandidate.Content = $"후보자 추가 완료 {DateTime.Now.Second}";
+        }
+    }
+}
---
+++ VisualAcademy.Desktop/VisualAcademy.Desktop/Rias/RiaGrid.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace VisualAcademy.Desktop.Rias
+{
+    /// <summary>
+    /// Interaction logic for RiaGrid.xaml
+    /// </summary>
+    public partial class RiaGrid : Window
+    {
+        public RiaGrid()
+        {
+            InitializeComponent();
+        }
+    }
+}
