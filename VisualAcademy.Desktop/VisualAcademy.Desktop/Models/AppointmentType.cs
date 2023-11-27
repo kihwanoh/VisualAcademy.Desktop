dd00beeb981e96335e226c6ab3c6b1fe566ad12c
kihwanoh <kihwan2003.oh@hanmail.net>
2023-11-27 22:06:47 +0900

    Create LoadData project

---
+++ VisualAcademy.Desktop/VisualAcademy.Desktop/Models/AppointmentType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#nullable disable
+namespace VisualAcademy.Desktop.Models
+{
+    public class AppointmentType
+    {
+        public int Id {get; set; }
+        public string AppointmentTypeName { get; set; }
+        public bool IdActive { get; set; } = true;
+        public DateTime DateCreated { get; set; } = DateTime.Now;
+    }
+}
